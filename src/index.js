f070029a3b4e99f3fd75c469fa6dfad22e7a289b
Di <azazarova99@mail.ru>
2020-03-08 23:51:53 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let n = str.length/2;
+  
+  for(let i = 0; i < n; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      str = str.replaceAll(bracketsConfig[j][0] + bracketsConfig[j][1], "");
+    }
+
+    if(str == "") {
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+String.prototype.replaceAll = function(search, replace){
+  return this.split(search).join(replace);
 }
